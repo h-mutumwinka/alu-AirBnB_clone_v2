fabe4e66db6e1b4fbab032aed9c7fcb431462fb0
h-mutumwinka <h.mutumwink@alustudent.com>
2025-06-20 02:41:16 -0700

    Add web static setup script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-echo "<h1>Enock Mugisha</h1>" > /data/web_static/releases/test/index.html
+echo "<h1Heroine Mutumwinka</h1>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
